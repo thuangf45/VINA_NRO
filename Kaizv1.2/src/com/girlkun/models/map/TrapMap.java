327e6a993ba441fdf2219ee1443df5e1e3b6c59f
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-26 22:37:48 +0700

    Update

--- Kaizv1.2/src/com/girlkun/models/map/TrapMap.java
+++ Kaizv1.2/src/com/girlkun/models/map/TrapMap.java
@@ -5,27 +5,47 @@ import com.girlkun.services.PlayerService;
 import com.girlkun.services.func.EffectMapService;
 import com.girlkun.utils.Util;
 
-
+/**
+ * Đại diện cho các bẫy (trap) trên bản đồ có thể gây hiệu ứng hoặc sát thương cho người chơi
+ * 
+ * @author Lucifer
+ */
 public class TrapMap {
 
+    /** Tọa độ X của bẫy */
     public int x;
+
+    /** Tọa độ Y của bẫy */
     public int y;
+
+    /** Chiều rộng của bẫy */
     public int w;
+
+    /** Chiều cao của bẫy */
     public int h;
+
+    /** ID hiệu ứng của bẫy */
     public int effectId;
+
+    /** Lượng sát thương cơ bản của bẫy */
     public int dame;
 
+    /**
+     * Xử lý khi người chơi va chạm với bẫy
+     *
+     * @param player Người chơi bị tác động bởi bẫy
+     */
     public void doPlayer(Player player) {
         switch (this.effectId) {
             case 49:
-    if (!player.isDie() && Util.canDoWithTime(player.iDMark.getLastTimeAnXienTrapBDKB(), 1000) && !player.isBoss) {
-        int increasedDame = dame * 2; // Tăng lượng dame lên 100%
-        player.injured(null, increasedDame + (Util.nextInt(-10, 10) * increasedDame / 100), false, false);
-        PlayerService.gI().sendInfoHp(player);
-        EffectMapService.gI().sendEffectMapToAllInMap(player.zone, effectId, 2, 1, player.location.x - 32, 1040, 1);
-        player.iDMark.setLastTimeAnXienTrapBDKB(System.currentTimeMillis());
-    }
-    break;
+                if (!player.isDie() && Util.canDoWithTime(player.iDMark.getLastTimeAnXienTrapBDKB(), 1000) && !player.isBoss) {
+                    int increasedDame = dame * 2; // Tăng lượng dame lên 100%
+                    player.injured(null, increasedDame + (Util.nextInt(-10, 10) * increasedDame / 100), false, false);
+                    PlayerService.gI().sendInfoHp(player);
+                    EffectMapService.gI().sendEffectMapToAllInMap(player.zone, effectId, 2, 1, player.location.x - 32, 1040, 1);
+                    player.iDMark.setLastTimeAnXienTrapBDKB(System.currentTimeMillis());
+                }
+                break;
         }
     }
 
