bfc2c4ee34ecf06e1815b7b4b5adea118d0c79f9
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-26 13:27:48 +0700

    Update IBossDie.java

--- Kaizv1.2/src/com/girlkun/models/boss/iboss/IBossDie.java
+++ Kaizv1.2/src/com/girlkun/models/boss/iboss/IBossDie.java
@@ -1,58 +1,33 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 package com.girlkun.models.boss.iboss;
 
 import com.girlkun.models.player.Player;
 
-
+/**
+ * Interface định nghĩa các hành vi liên quan đến sự kiện khi boss chết trong game.
+ * @author Lucifer
+ */
 public interface IBossDie {
     
+    /**
+     * Thực hiện một số hành động cụ thể khi boss chết, chẳng hạn như xử lý logic đặc biệt.
+     * @param playerKill Người chơi đã tiêu diệt boss
+     */
     void doSomeThing(Player playerKill);
 
+    /**
+     * Thông báo rằng boss đã chết tới các đối tượng hoặc hệ thống liên quan.
+     * @param playerKill Người chơi đã tiêu diệt boss
+     */
     void notifyDie(Player playerKill);
 
+    /**
+     * Xử lý phần thưởng cho người chơi khi tiêu diệt boss.
+     * @param playerKill Người chơi đã tiêu diệt boss
+     */
     void rewards(Player playerKill);
 
+    /**
+     * Xóa boss khỏi bản đồ sau khi chết.
+     */
     void leaveMap();
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
