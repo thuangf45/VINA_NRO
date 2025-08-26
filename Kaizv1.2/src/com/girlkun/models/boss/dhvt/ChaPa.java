60c5da46011eef021b877b4179df5c2b19516d9b
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-26 12:48:29 +0700

    Update Chapa

--- Kaizv1.2/src/com/girlkun/models/boss/dhvt/ChaPa.java
+++ Kaizv1.2/src/com/girlkun/models/boss/dhvt/ChaPa.java
@@ -4,13 +4,17 @@ import com.girlkun.models.boss.BossData;
 import com.girlkun.models.boss.BossID;
 import com.girlkun.models.boss.BossesData;
 import com.girlkun.models.player.Player;
+
 /**
- * @author BTH sieu cap vippr0 
+ * Lớp đại diện cho boss ChaPa trong sự kiện Đại Hội Võ Thuật (DHVT)
+ * @author Lucifer
  */
 public class ChaPa extends BossDHVT {
 
+    /** Constructor khởi tạo boss ChaPa với ID và dữ liệu từ BossesData */
     public ChaPa(Player player) throws Exception {
         super(BossID.CHA_PA, BossesData.CHA_PA);
+        /** Gán người chơi mục tiêu cho boss */
         this.playerAtt = player;
     }
 }
