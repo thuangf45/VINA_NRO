7440a6f383abc6bd685fd7979dbbc4f8e807f60e
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-26 12:49:36 +0700

    Update ChanXu

--- Kaizv1.2/src/com/girlkun/models/boss/dhvt/ChanXu.java
+++ Kaizv1.2/src/com/girlkun/models/boss/dhvt/ChanXu.java
@@ -4,13 +4,17 @@ import com.girlkun.models.boss.BossData;
 import com.girlkun.models.boss.BossID;
 import com.girlkun.models.boss.BossesData;
 import com.girlkun.models.player.Player;
+
 /**
- * @author BTH sieu cap vippr0 
+ * Lớp đại diện cho boss ChanXu trong sự kiện Đại Hội Võ Thuật (DHVT)
+ * @author Lucifer
  */
 public class ChanXu extends BossDHVT {
 
+    /** Constructor khởi tạo boss ChanXu với ID và dữ liệu từ BossesData */
     public ChanXu(Player player) throws Exception {
         super(BossID.CHAN_XU, BossesData.CHAN_XU);
+        /** Gán người chơi mục tiêu cho boss */
         this.playerAtt = player;
     }
 }
