b133fd009dbce696ee8b32f6f1fc2fbff61c1e30
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-27 00:02:35 +0700

    Update PVPDaiHoi

--- Kaizv1.2/src/com/girlkun/models/matches/pvp/PVPDaiHoi.java
+++ Kaizv1.2/src/com/girlkun/models/matches/pvp/PVPDaiHoi.java
@@ -9,55 +9,90 @@ import com.girlkun.services.func.ChangeMapService;
 import com.girlkun.utils.Util;
 
 /**
- *
- * @author Administrator
+ * Lớp đại diện cho trận đấu PVP trong sự kiện Đại Hội Võ Thuật.
+ * @author Lucifer
  */
-public class PVPDaiHoi extends PVP{
+public class PVPDaiHoi extends PVP {
+
+    /**
+     * Lượng vàng cược cho trận đấu.
+     */
     private final int goldThachDau;
+
+    /**
+     * Lượng vàng phần thưởng khi thắng trận đấu.
+     */
     private final long goldReward;
+
+    /**
+     * Đối tượng Đại Hội Võ Thuật liên quan đến trận đấu.
+     */
     private final DaiHoiVoThuat dh;
+
+    /**
+     * Thời gian bắt đầu trận đấu PVP.
+     */
     private long lastTimePVP;
 
-    public PVPDaiHoi(Player p1, Player p2, int goldThachDau , DaiHoiVoThuat d , long l) {
+    /**
+     * Constructor khởi tạo trận đấu PVP trong Đại Hội Võ Thuật.
+     * @param p1 Người chơi thứ nhất
+     * @param p2 Người chơi thứ hai
+     * @param goldThachDau Lượng vàng cược cho trận đấu
+     * @param d Đối tượng Đại Hội Võ Thuật
+     * @param l Thời gian bắt đầu trận đấu
+     */
+    public PVPDaiHoi(Player p1, Player p2, int goldThachDau, DaiHoiVoThuat d, long l) {
         super(TYPE_PVP.DAI_HOI_VO_THUAT, p1, p2);
         this.goldThachDau = goldThachDau;
         this.goldReward = goldThachDau;
-        dh = d;
-        lastTimePVP = l;
+        this.dh = d;
+        this.lastTimePVP = l;
     }
 
+    /**
+     * Bắt đầu trận đấu PVP trong Đại Hội Võ Thuật.
+     */
     @Override
     public void start() {
         super.start();
     }
 
+    /**
+     * Kết thúc trận đấu PVP (không thực hiện hành động cụ thể trong lớp này).
+     */
     @Override
     public void finish() {
-
     }
 
+    /**
+     * Dọn dẹp tài nguyên và kết thúc trận đấu PVP.
+     */
     @Override
     public void dispose() {
         super.dispose();
     }
 
+    /**
+     * Cập nhật trạng thái trận đấu PVP, kiểm tra thời gian và xác định người thắng dựa trên HP hoặc hòa.
+     */
     @Override
     public void update() {
-        if(Util.canDoWithTime(lastTimePVP, 180000)){
-            if(p1.nPoint.hp > p2.nPoint.hp){
-                ChangeMapService.gI().changeMapBySpaceShip(p2,21 +p2.gender, 0, -1);
+        if (Util.canDoWithTime(lastTimePVP, 180000)) {
+            if (p1.nPoint.hp > p2.nPoint.hp) {
+                ChangeMapService.gI().changeMapBySpaceShip(p2, 21 + p2.gender, 0, -1);
                 p1.pvp.lose(p2, TYPE_LOSE_PVP.DEAD);
-            }else if(p2.nPoint.hp > p1.nPoint.hp){
-                ChangeMapService.gI().changeMapBySpaceShip(p1,21 + p1.gender, 0, -1);
+            } else if (p2.nPoint.hp > p1.nPoint.hp) {
+                ChangeMapService.gI().changeMapBySpaceShip(p1, 21 + p1.gender, 0, -1);
                 p2.pvp.lose(p1, TYPE_LOSE_PVP.DEAD);
-            }else{
+            } else {
                 Service.gI().sendThongBao(p1, "Trận đấu hòa. Bạn nhận được " + Util.numberToMoney(this.goldReward) + " vàng");
                 p1.inventory.gold += this.goldReward / 2;
                 Service.gI().sendMoney(p1);
                 dh.listReg.add(p1);
                 p1.lastTimeWin = System.currentTimeMillis();
                 p1.isWin = true;
-                
+
                 Service.gI().sendThongBao(p2, "Trận đấu hòa. Bạn nhận được " + Util.numberToMoney(this.goldReward) + " vàng");
                 p2.inventory.gold += this.goldReward / 2;
                 Service.gI().sendMoney(p2);
@@ -68,6 +103,10 @@ public class PVPDaiHoi extends PVP{
         }
     }
 
+    /**
+     * Trao phần thưởng cho người chơi thắng trận đấu.
+     * @param plWin Người chơi thắng
+     */
     @Override
     public void reward(Player plWin) {
         plWin.inventory.gold += this.goldReward;
@@ -77,6 +116,11 @@ public class PVPDaiHoi extends PVP{
         plWin.isWin = true;
     }
 
+    /**
+     * Gửi kết quả trận đấu và xử lý phần thưởng hoặc hình phạt cho người chơi thua.
+     * @param plLose Người chơi thua
+     * @param typeLose Loại thua cuộc (bỏ chạy hoặc chết)
+     */
     @Override
     public void sendResult(Player plLose, TYPE_LOSE_PVP typeLose) {
         if (typeLose == TYPE_LOSE_PVP.RUNS_AWAY) {
