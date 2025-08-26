30732b4b34120e366fe9a850d05e257968cd1405
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-26 13:22:22 +0700

    Update Yamcha.java

--- Kaizv1.2/src/com/girlkun/models/boss/dhvt/Yamcha.java
+++ Kaizv1.2/src/com/girlkun/models/boss/dhvt/Yamcha.java
@@ -1,19 +1,23 @@
 package com.girlkun.models.boss.dhvt;
 
+import com.girlkun.models.boss.BossData;
 import com.girlkun.models.boss.BossID;
 import com.girlkun.models.boss.BossesData;
-import com.girlkun.models.boss.dhvt.BossDHVT;
 import com.girlkun.models.player.Player;
 
-
 /**
- *
- * @author BTH fix
+ * Lớp đại diện cho boss JackyChun trong sự kiện Đại Hội Võ Thuật (DHVT)
+ * @author Lucifer
  */
-public class Yamcha extends BossDHVT {
+public class JackyChun extends BossDHVT {
 
-    public Yamcha(Player player) throws Exception {
-        super(BossID.YAMCHA, BossesData.YAMCHA);
+    /**
+     * Constructor khởi tạo boss JackyChun với ID và dữ liệu từ BossesData
+     * @param player Người chơi mục tiêu mà boss sẽ tấn công
+     */
+    public JackyChun(Player player) throws Exception {
+        super(BossID.JACKY_CHUN, BossesData.JACKY_CHUN);
+        /** Gán người chơi mục tiêu cho boss */
         this.playerAtt = player;
     }
 }
