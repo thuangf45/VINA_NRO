5e2cc05bd22961809780d62d75d33476731d4c4b
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-26 16:14:39 +0700

    Update TrungUyThep

--- Kaizv1.2/src/com/girlkun/models/boss/list_boss/doanh_trai/TrungUyThep.java
+++ Kaizv1.2/src/com/girlkun/models/boss/list_boss/doanh_trai/TrungUyThep.java
@@ -1,4 +1,5 @@
 package com.girlkun.models.boss.list_boss.doanh_trai;
+
 import com.girlkun.consts.ConstPlayer;
 import com.girlkun.models.boss.*;
 import static com.girlkun.models.boss.BossStatus.ACTIVE;
@@ -17,53 +18,101 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * @author trùm
+ * Lớp TrungUyThep đại diện cho boss "Trung Uý Thép" trong game. 
+ * Boss này xuất hiện ở map Doanh trại với đặc điểm công - thủ riêng biệt.  
+ * 
+ * - Có kỹ năng đa dạng: Demon, Dragon, Galick, Kamejoko, Antomic, Masenko.  
+ * - Tự động rời map sau thời gian tồn tại.  
+ * - Giảm sát thương nhận vào khi bị tấn công.  
+ * - Khi bị hạ gục sẽ rơi vật phẩm.  
+ *
+ * @author Lucifer
  */
 public class TrungUyThep extends Boss {
-    private static final int[][] FULL_DEMON = new int[][]{{Skill.DEMON, 1}, {Skill.DEMON, 2}, {Skill.DEMON, 3}, {Skill.DEMON, 4}, {Skill.DEMON, 5}, {Skill.DEMON, 6}, {Skill.DEMON, 7}};
-  private long lastTimeHapThu;
+
+    /** Bộ kỹ năng DEMON đầy đủ các cấp */
+    private static final int[][] FULL_DEMON = new int[][]{
+        {Skill.DEMON, 1}, {Skill.DEMON, 2}, {Skill.DEMON, 3},
+        {Skill.DEMON, 4}, {Skill.DEMON, 5}, {Skill.DEMON, 6}, {Skill.DEMON, 7}
+    };
+
+    private long lastTimeHapThu;
     private int timeHapThu;
     private int initSuper = 0;
     protected Player playerAtt;
     private int timeLive = 10;
-    public TrungUyThep(Zone zone , int dame, int hp ) throws Exception {
+    private long st;
+
+    /**
+     * Khởi tạo boss Trung Uý Thép.  
+     *
+     * @param zone khu vực boss sẽ xuất hiện
+     * @param dame sát thương cơ bản
+     * @param hp máu cơ bản
+     * @throws Exception nếu có lỗi khởi tạo
+     */
+    public TrungUyThep(Zone zone, int dame, int hp) throws Exception {
         super(BossID.TRUNG_UY_THEP, new BossData(
                 "Trung Uý Thép", //name
                 ConstPlayer.TRAI_DAT, //gender
                 new short[]{129, 130, 131, -1, -1, -1}, //outfit {head, body, leg, bag, aura, eff}
-                ((hp/6 )), //dame
-                new int[]{((dame * 50 ))}, //hp
+                ((hp / 6)), //dame
+                new int[]{((dame * 50))}, //hp
                 new int[]{55}, //map join
                 new int[][]{
-                {Skill.DEMON, 3, 1}, {Skill.DEMON, 6, 2}, {Skill.DRAGON, 7, 3}, {Skill.DRAGON, 1, 4}, {Skill.GALICK, 5, 5},
-                {Skill.KAMEJOKO, 7, 6}, {Skill.KAMEJOKO, 6, 7}, {Skill.KAMEJOKO, 5, 8}, {Skill.KAMEJOKO, 4, 9}, {Skill.KAMEJOKO, 3, 10}, {Skill.KAMEJOKO, 2, 11},{Skill.KAMEJOKO, 1, 12},
-              {Skill.ANTOMIC, 1, 13},  {Skill.ANTOMIC, 2, 14},  {Skill.ANTOMIC, 3, 15},{Skill.ANTOMIC, 4, 16},  {Skill.ANTOMIC, 5, 17},{Skill.ANTOMIC, 6, 19},  {Skill.ANTOMIC, 7, 20},
-                {Skill.MASENKO, 1, 21}, {Skill.MASENKO, 5, 22}, {Skill.MASENKO, 6, 23},
+                    {Skill.DEMON, 3, 1}, {Skill.DEMON, 6, 2}, {Skill.DRAGON, 7, 3},
+                    {Skill.DRAGON, 1, 4}, {Skill.GALICK, 5, 5},
+                    {Skill.KAMEJOKO, 7, 6}, {Skill.KAMEJOKO, 6, 7},
+                    {Skill.KAMEJOKO, 5, 8}, {Skill.KAMEJOKO, 4, 9},
+                    {Skill.KAMEJOKO, 3, 10}, {Skill.KAMEJOKO, 2, 11},
+                    {Skill.KAMEJOKO, 1, 12},
+                    {Skill.ANTOMIC, 1, 13}, {Skill.ANTOMIC, 2, 14},
+                    {Skill.ANTOMIC, 3, 15}, {Skill.ANTOMIC, 4, 16},
+                    {Skill.ANTOMIC, 5, 17}, {Skill.ANTOMIC, 6, 19},
+                    {Skill.ANTOMIC, 7, 20},
+                    {Skill.MASENKO, 1, 21}, {Skill.MASENKO, 5, 22},
+                    {Skill.MASENKO, 6, 23},
                     {Skill.KAMEJOKO, 7, 1000},},
                 new String[]{}, //text chat 1
                 new String[]{"|-1|Nhóc con"}, //text chat 2
                 new String[]{}, //text chat 3
                 60
         ));
-        
         this.zone = zone;
     }
+
+    /**
+     * Xử lý phần thưởng khi người chơi hạ gục boss.  
+     * 100% rơi item có id = 18.
+     *
+     * @param plKill người chơi hạ boss
+     */
     @Override
     public void reward(Player plKill) {
         if (Util.isTrue(100, 100)) {
-            ItemMap it = new ItemMap(this.zone, 18, 1, this.location.x, this.zone.map.yPhysicInTop(this.location.x,
-                    this.location.y - 24), plKill.id);
+            ItemMap it = new ItemMap(this.zone, 18, 1,
+                    this.location.x,
+                    this.zone.map.yPhysicInTop(this.location.x, this.location.y - 24),
+                    plKill.id);
             Service.getInstance().dropItemMap(this.zone, it);
         }
     }
-    
-    
+
+    /**
+     * Hành vi hoạt động của boss.  
+     * Nếu quá 30 phút sẽ rời map.
+     */
     public void active() {
-        super.active(); //To change body of generated methods, choose Tools | Templates.
+        super.active();
         if (Util.canDoWithTime(st, 1800000)) {
             this.changeStatus(BossStatus.LEAVE_MAP);
         }
     }
+
+    /**
+     * Boss rời khỏi map.  
+     * Sau khi đủ 30 phút thì bị xoá khỏi BossManager.
+     */
     @Override
     public void leaveMap() {
         super.leaveMap();
@@ -71,25 +120,41 @@ public class TrungUyThep extends Boss {
             BossManager.gI().removeBoss(this);
         }
     }
+
+    /**
+     * Boss tham gia map.  
+     * Lưu lại thời điểm bắt đầu xuất hiện.
+     */
     @Override
     public void joinMap() {
-        super.joinMap(); //To change body of generated methods, choose Tools | Templates.
+        super.joinMap();
         st = System.currentTimeMillis();
     }
-    private long st;
-    
+
+    /**
+     * Xử lý khi boss nhận sát thương từ người chơi hoặc mob.  
+     * - Có tỉ lệ né đòn.  
+     * - Nếu có khiên, giảm sát thương.  
+     * - Khi hết máu sẽ chết.  
+     *
+     * @param plAtt người tấn công
+     * @param damage sát thương gây ra
+     * @param piercing có xuyên giáp hay không
+     * @param isMobAttack có phải mob tấn công hay không
+     * @return sát thương thực tế boss nhận
+     */
     public int injured(Player plAtt, int damage, boolean piercing, boolean isMobAttack) {
         if (!this.isDie()) {
             if (!piercing && Util.isTrue(this.nPoint.tlNeDon, 1000)) {
                 this.chat("Xí hụt");
                 return 0;
             }
-            damage = this.nPoint.subDameInjureWithDeff(damage/2);
+            damage = this.nPoint.subDameInjureWithDeff(damage / 2);
             if (!piercing && effectSkill.isShielding) {
                 if (damage > nPoint.hpMax) {
                     EffectSkillService.gI().breakShield(this);
                 }
-                damage = damage/2;
+                damage = damage / 2;
             }
             this.nPoint.subHP(damage);
             if (isDie()) {
@@ -101,13 +166,4 @@ public class TrungUyThep extends Boss {
             return 0;
         }
     }
-    
-    
 }
-
-
-
-
-
-
-
