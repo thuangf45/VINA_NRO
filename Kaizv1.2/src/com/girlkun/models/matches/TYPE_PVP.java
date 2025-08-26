d8f400bae196c2969f33c2ee5bafba929d4f03b2
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-26 23:44:43 +0700

    Update TYPE_PVP

--- Kaizv1.2/src/com/girlkun/models/matches/TYPE_PVP.java
+++ Kaizv1.2/src/com/girlkun/models/matches/TYPE_PVP.java
@@ -1,31 +1,23 @@
 package com.girlkun.models.matches;
 
-
+/**
+ * Enum định nghĩa các loại trận đấu PVP trong game.
+ * @author Lucifer
+ */
 public enum TYPE_PVP {
 
+    /**
+     * Loại trận đấu thách đấu giữa hai người chơi.
+     */
     THACH_DAU,
+
+    /**
+     * Loại trận đấu trả thù để đối đầu với kẻ thù.
+     */
     TRA_THU,
+
+    /**
+     * Loại trận đấu trong sự kiện Đại Hội Võ Thuật.
+     */
     DAI_HOI_VO_THUAT
-    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
