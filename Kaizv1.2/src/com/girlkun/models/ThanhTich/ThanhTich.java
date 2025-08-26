fe49f0fe564bf1a4db2e6b88196d588b8c61ef81
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-26 10:09:40 +0700

    Update ThanhTich

--- Kaizv1.2/src/com/girlkun/models/ThanhTich/ThanhTich.java
+++ Kaizv1.2/src/com/girlkun/models/ThanhTich/ThanhTich.java
@@ -1,27 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.girlkun.models.ThanhTich;
 
 import com.girlkun.models.Template;
 
 /**
- *
- * @author Administrator
+ * Đại diện cho một thành tích (achievement) của người chơi.
+ * - Lưu template định nghĩa (nội dung, phần thưởng, điều kiện...).
+ * - Quản lý trạng thái đã hoàn thành hay chưa (isFinish).
+ * - Quản lý trạng thái đã nhận thưởng hay chưa (isRecieve).
+ * 
+ * @author Lucifer
  */
-public class ThanhTich 
-{
+public class ThanhTich {
+    
+    /** Thông tin template gốc của thành tích */
     public Template.ArchivementTemplate Template;
+
+    /** Đã hoàn thành thành tích chưa */
     public boolean isFinish;
+
+    /** Đã nhận thưởng thành tích chưa */
     public boolean isRecieve;
-    public ThanhTich()
-    {
-        
-    }
-    public ThanhTich(ThanhTich a)
-    {
+
+    /** Constructor mặc định */
+    public ThanhTich() { }
+
+    /** Constructor sao chép từ 1 thành tích khác */
+    public ThanhTich(ThanhTich a) {
         this.Template = a.Template;
         this.isFinish = a.isFinish;
         this.isRecieve = a.isRecieve;
