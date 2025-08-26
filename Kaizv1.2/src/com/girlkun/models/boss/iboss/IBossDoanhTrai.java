68c10bb5a6791df08659dbc9aad0292fef7d6e74
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-26 13:29:46 +0700

    Update IBossDoanhTrai.java

--- Kaizv1.2/src/com/girlkun/models/boss/iboss/IBossDoanhTrai.java
+++ Kaizv1.2/src/com/girlkun/models/boss/iboss/IBossDoanhTrai.java
@@ -1,27 +1,9 @@
 package com.girlkun.models.boss.iboss;
 
-
+/**
+ * Interface định nghĩa các hành vi liên quan đến boss trong sự kiện Doanh Trại trong game.
+ * @author Lucifer
+ */
 public interface IBossDoanhTrai {
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
