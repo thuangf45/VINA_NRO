98e35454002e0562f506c3b9b59bb390bc6665a9
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-26 13:22:05 +0700

    Update LiuLiu.java

--- Kaizv1.2/src/com/girlkun/models/boss/dhvt/LiuLiu.java
+++ Kaizv1.2/src/com/girlkun/models/boss/dhvt/LiuLiu.java
@@ -4,13 +4,17 @@ import com.girlkun.models.boss.BossData;
 import com.girlkun.models.boss.BossID;
 import com.girlkun.models.boss.BossesData;
 import com.girlkun.models.player.Player;
+
 /**
- * @author BTH sieu cap vippr0 
+ * Lớp đại diện cho boss LiuLiu trong sự kiện Đại Hội Võ Thuật (DHVT)
+ * @author Lucifer
  */
 public class LiuLiu extends BossDHVT {
 
+    /** Constructor khởi tạo boss LiuLiu với ID và dữ liệu từ BossesData */
     public LiuLiu(Player player) throws Exception {
         super(BossID.LIU_LIU, BossesData.LIU_LIU);
+        /** Gán người chơi mục tiêu cho boss */
         this.playerAtt = player;
     }
 }
