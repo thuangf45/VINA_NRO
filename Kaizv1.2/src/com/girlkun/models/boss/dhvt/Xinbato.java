a9a8455287e01f88ccc6422b06f755da55557258
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-26 13:22:20 +0700

    Update Xinbato.java

--- Kaizv1.2/src/com/girlkun/models/boss/dhvt/Xinbato.java
+++ Kaizv1.2/src/com/girlkun/models/boss/dhvt/Xinbato.java
@@ -1,16 +1,23 @@
 package com.girlkun.models.boss.dhvt;
 
+import com.girlkun.models.boss.BossData;
 import com.girlkun.models.boss.BossID;
 import com.girlkun.models.boss.BossesData;
 import com.girlkun.models.player.Player;
 
 /**
- * @author BTH sieu cap vippr0 
+ * Lớp đại diện cho boss Xinbato trong sự kiện Đại Hội Võ Thuật (DHVT)
+ * @author Lucifer
  */
 public class Xinbato extends BossDHVT {
 
+    /**
+     * Constructor khởi tạo boss Xinbato với ID và dữ liệu từ BossesData
+     * @param player Người chơi mục tiêu mà boss sẽ tấn công
+     */
     public Xinbato(Player player) throws Exception {
         super(BossID.XINBATO, BossesData.XINBATO);
+        /** Gán người chơi mục tiêu cho boss */
         this.playerAtt = player;
     }
 }
