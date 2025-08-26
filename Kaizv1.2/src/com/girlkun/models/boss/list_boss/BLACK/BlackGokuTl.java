45c75cc05fde22be52c17484ff8580412556e275
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-26 13:58:06 +0700

    Update BlackGokuTl

--- Kaizv1.2/src/com/girlkun/models/boss/list_boss/BLACK/BlackGokuTl.java
+++ Kaizv1.2/src/com/girlkun/models/boss/list_boss/BLACK/BlackGokuTl.java
@@ -8,19 +8,37 @@ import com.girlkun.services.EffectSkillService;
 import com.girlkun.services.Service;
 import com.girlkun.utils.Util;
 
-import java.util.Random;
-
+/**
+ * Lớp đại diện cho boss Black Goku TL trong game.
+ * @author Lucifer
+ */
 public class BlackGokuTl extends Boss {
 
+    /**
+     * Constructor khởi tạo boss Black Goku TL với ID và dữ liệu từ BossesData.
+     * @throws Exception Nếu có lỗi trong quá trình khởi tạo
+     */
     public BlackGokuTl() throws Exception {
         super(BossID.BLACK1, BossesData.BLACK_GOKU, BossesData.SUPER_BLACK_GOKU);
     }
 
+    /**
+     * Xử lý phần thưởng khi người chơi tiêu diệt boss Black Goku TL.
+     * @param plKill Người chơi đã tiêu diệt boss
+     */
     @Override
     public void reward(Player plKill) {
-         rewardFutureBoss(plKill);
+        rewardFutureBoss(plKill);
     }
 
+    /**
+     * Xử lý sát thương mà boss Black Goku TL nhận từ người chơi hoặc quái.
+     * @param plAtt Người chơi tấn công boss
+     * @param damage Lượng sát thương gây ra
+     * @param piercing True nếu sát thương bỏ qua phòng thủ
+     * @param isMobAttack True nếu sát thương đến từ quái
+     * @return Lượng sát thương thực tế được áp dụng
+     */
     @Override
     public int injured(Player plAtt, int damage, boolean piercing, boolean isMobAttack) {
         if (!this.isDie()) {
@@ -46,43 +64,70 @@ public class BlackGokuTl extends Boss {
         }
     }
 
+    /**
+     * Kích hoạt hành vi của boss Black Goku TL, bao gồm kiểm tra thời gian để rời bản đồ.
+     */
     @Override
     public void active() {
-        super.active(); //To change body of generated methods, choose Tools | Templates.
+        super.active();
         if (Util.canDoWithTime(st, 900000000)) {
-            //   this.changeStatus(BossStatus.LEAVE_MAP);
+             this.changeStatus(BossStatus.LEAVE_MAP);
         }
     }
 
+    /**
+     * Thêm boss Black Goku TL vào bản đồ và ghi lại thời gian bắt đầu.
+     */
     @Override
     public void joinMap() {
-        super.joinMap(); //To change body of generated methods, choose Tools | Templates.
+        super.joinMap();
         st = System.currentTimeMillis();
     }
 
+    /**
+     * Thời gian bắt đầu khi boss tham gia bản đồ.
+     */
     private long st;
 
-//    @Override
-//    public void moveTo(int x, int y) {
-//        if(this.currentLevel == 1){
-//            return;
-//        }
-//        super.moveTo(x, y);
-//    }
-//
-//    @Override
-//    public void reward(Player plKill) {
-//        if(this.currentLevel == 1){
-//            return;
-//        }
-//        super.reward(plKill);
-//    }
-//
-//    @Override
-//    protected void notifyJoinMap() {
-//        if(this.currentLevel == 1){
-//            return;
-//        }
-//        super.notifyJoinMap();
-//    }
+    /**
+     * Di chuyển boss Black Goku TL đến tọa độ cụ thể (bị vô hiệu hóa nếu ở cấp độ 1).
+     * @param x Tọa độ X
+     * @param y Tọa độ Y
+     */
+    /*
+    @Override
+    public void moveTo(int x, int y) {
+        if (this.currentLevel == 1) {
+            return;
+        }
+        super.moveTo(x, y);
+    }
+    */
+
+    /**
+     * Xử lý phần thưởng khi người chơi tiêu diệt boss (bị vô hiệu hóa nếu ở cấp độ 1).
+     * @param plKill Người chơi đã tiêu diệt boss
+     */
+    /*
+    @Override
+    public void reward(Player plKill) {
+        if (this.currentLevel == 1) {
+            return;
+        }
+        super.reward(plKill);
+    }
+    */
+
+    /**
+     * Thông báo khi boss Black Goku TL tham gia bản đồ (bị vô hiệu hóa nếu ở cấp độ 1).
+     */
+    /*
+    @Override
+    protected void notifyJoinMap() {
+        if (this.currentLevel == 1) {
+            return;
+        }
+        super.notifyJoinMap();
+    }
+    */
 }
