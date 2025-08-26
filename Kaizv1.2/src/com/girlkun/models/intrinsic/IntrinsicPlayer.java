c97ac6855d2c8e0bcff3874a9cb4be2ebfe3177c
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-26 22:11:04 +0700

    Update InstrinsicPlayer

--- Kaizv1.2/src/com/girlkun/models/intrinsic/IntrinsicPlayer.java
+++ Kaizv1.2/src/com/girlkun/models/intrinsic/IntrinsicPlayer.java
@@ -2,18 +2,43 @@ package com.girlkun.models.intrinsic;
 
 import com.girlkun.services.IntrinsicService;
 
-
+/**
+ * Đại diện cho nội tại hiện tại của một người chơi.
+ * <p>
+ * Mỗi người chơi có thể sở hữu và mở nhiều lần nội tại. 
+ * Nội tại được quản lý thông qua {@link IntrinsicService}.
+ * </p>
+ * 
+ * @author Lucifer
+ */
 public class IntrinsicPlayer {
 
+    /** 
+     * Số lần người chơi đã mở nội tại (dùng để giới hạn/tính xác suất). 
+     */
     public byte countOpen;
 
+    /** 
+     * Nội tại hiện tại của người chơi. 
+     */
     public Intrinsic intrinsic;
 
+    /**
+     * Constructor mặc định. 
+     * <p>
+     * Khi khởi tạo, nội tại mặc định sẽ là loại có ID = 0, 
+     * được lấy từ {@link IntrinsicService}.
+     * </p>
+     */
     public IntrinsicPlayer() {
         this.intrinsic = IntrinsicService.gI().getIntrinsicById(0);
     }
 
-    public void dispose(){
+    /**
+     * Giải phóng dữ liệu, tránh rò rỉ bộ nhớ.
+     * Sau khi gọi, {@link #intrinsic} sẽ bị gán về {@code null}.
+     */
+    public void dispose() {
         this.intrinsic = null;
     }
 }
