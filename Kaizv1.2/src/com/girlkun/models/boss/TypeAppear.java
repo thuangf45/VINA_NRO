d3db1d8e8d8c6236563ceee9e9ffeae0dd01ba09
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-26 10:37:47 +0700

    Update TypeAppear

--- Kaizv1.2/src/com/girlkun/models/boss/TypeAppear.java
+++ Kaizv1.2/src/com/girlkun/models/boss/TypeAppear.java
@@ -1,16 +1,22 @@
 package com.girlkun.models.boss;
 
-
+/**
+ * Kiểu xuất hiện (cách spawn) của Boss.
+ * Dùng để xác định Boss xuất hiện độc lập, đi kèm hay do Boss khác gọi ra.
+ *
+ * @author Lucifer
+ */
 public enum TypeAppear {
 
+    /** Boss xuất hiện mặc định (bình thường, theo lịch hoặc điều kiện game) */
     DEFAULT_APPEAR,
-    APPEAR_WITH_ANOTHER,
-    ANOTHER_LEVEL,
-    CALL_BY_ANOTHER
-    
-}
 
-/**
- * Vui lòng không sao chép mã nguồn này dưới mọi hình thức. Hãy tôn trọng tác
- * giả của mã nguồn này. Xin cảm ơn! - GirlBeo
- */
+    /** Xuất hiện cùng lúc với một Boss khác */
+    APPEAR_WITH_ANOTHER,
+
+    /** Phiên bản cấp độ khác (Boss mạnh hơn hoặc yếu hơn) */
+    ANOTHER_LEVEL,
+
+    /** Được gọi ra bởi một Boss khác trong trận chiến */
+    CALL_BY_ANOTHER
+}
