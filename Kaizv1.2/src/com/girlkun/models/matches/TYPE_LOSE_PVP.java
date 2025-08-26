4207b44e1a05696d8cb74a2a5fe86f75094011ba
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-26 23:43:40 +0700

    Update TYPE_LOSE_PVP

--- Kaizv1.2/src/com/girlkun/models/matches/TYPE_LOSE_PVP.java
+++ Kaizv1.2/src/com/girlkun/models/matches/TYPE_LOSE_PVP.java
@@ -1,31 +1,18 @@
 package com.girlkun.models.matches;
 
-
+/**
+ * Enum định nghĩa các loại thua cuộc trong trận đấu PVP.
+ * @author Lucifer
+ */
 public enum TYPE_LOSE_PVP {
 
+    /**
+     * Người chơi thua do bỏ chạy khỏi trận đấu.
+     */
     RUNS_AWAY,
+
+    /**
+     * Người chơi thua do bị hạ gục (chết).
+     */
     DEAD
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
