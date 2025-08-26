ec830f99fa427ab63fc4f85e9c7cdea3daa9ecd5
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-27 00:11:17 +0700

    Update ArrietyDrop

--- Kaizv1.2/src/com/girlkun/models/mob/ArrietyDrop.java
+++ Kaizv1.2/src/com/girlkun/models/mob/ArrietyDrop.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.girlkun.models.mob;
 
 import com.girlkun.models.item.Item;
@@ -17,46 +12,80 @@ import java.util.List;
 import java.util.Random;
 
 /**
- *
- * @author MITCHIKEN ZALO 0358689793
+ * Lớp quản lý việc thả (drop) các vật phẩm phần thưởng trong game.
+ * @author Lucifer
  */
 public class ArrietyDrop {
 
-   // public static final int[] list_thuc_an = new int[]{663, 664, 665, 666, 667};
-
+    /**
+     * Danh sách các vật phẩm đồ hủy diệt cho các hành tinh (Trái Đất, Namec, Xayda).
+     */
     public static final int[][] list_do_huy_diet = {
-        {650, 651, 657, 658, 656},
-        {652, 653, 659, 660, 656},
-        {654, 655, 661, 662, 656}}; //td, namec,xd
+        {650, 651, 657, 658, 656}, // Trái Đất
+        {652, 653, 659, 660, 656}, // Namec
+        {654, 655, 661, 662, 656}  // Xayda
+    };
 
+    /**
+     * Thả một vật phẩm phần thưởng tại vị trí xác định.
+     * @param player Người chơi nhận vật phẩm
+     * @param idItem ID của vật phẩm
+     * @param soluong Số lượng vật phẩm
+     * @param x Tọa độ X
+     * @param y Tọa độ Y
+     */
     public static void DropItemReWard(Player player, int idItem, int soluong, int x, int y) {
         ItemMap item = new ItemMap(player.zone, idItem, soluong, Util.nextInt((x - 50), (x + 50)), y, player.id);
         item.options.add(new Item.ItemOption(30, 0));
         Service.getInstance().dropItemMap(player.zone, item);
-        if(player.charms.tdThuHut > System.currentTimeMillis())
-        {
-             ItemMapService.gI().pickItem(player, item.itemMapId, true);
+        if (player.charms.tdThuHut > System.currentTimeMillis()) {
+            ItemMapService.gI().pickItem(player, item.itemMapId, true);
         }
     }
 
+    /**
+     * Thả vật phẩm đồ hủy diệt kích hoạt tại vị trí xác định.
+     * @param player Người chơi nhận vật phẩm
+     * @param soluong Số lượng vật phẩm
+     * @param x Tọa độ X
+     * @param y Tọa độ Y
+     */
     public static void DropItemReWardDoHuyDietKichHoat(Player player, int soluong, int x, int y) {
         Item itemHuyDiet = ArrietyDrop.randomCS_DHD(ArrietyDrop.list_do_huy_diet[player.gender][Util.nextInt(0, 4)], player.gender);
-        ItemMap item = new ItemMap(player.zone,itemHuyDiet.template.id, soluong, Util.nextInt((x - 50), (x + 50)), y, player.id);
-        item.options = itemHuyDiet.itemOptions;      
+        ItemMap item = new ItemMap(player.zone, itemHuyDiet.template.id, soluong, Util.nextInt((x - 50), (x + 50)), y, player.id);
+        item.options = itemHuyDiet.itemOptions;
         Service.getInstance().dropItemMap(player.zone, item);
-
     }
+
+    /**
+     * Thả mảnh thiên sứ tại vị trí xác định.
+     * @param player Người chơi nhận vật phẩm
+     * @param soluong Số lượng mảnh thiên sứ
+     * @param x Tọa độ X
+     * @param y Tọa độ Y
+     */
     public static void DropManhThienSu(Player player, int soluong, int x, int y) {
         Item mts = ItemService.gI().createNewItem((short) Util.nextInt(1066, 1070));
-        ItemMap item = new ItemMap(player.zone,mts.template.id, soluong, Util.nextInt((x - 50), (x + 50)), y, player.id);
-        item.options.add(new Item.ItemOption(73, 0));  
+        ItemMap item = new ItemMap(player.zone, mts.template.id, soluong, Util.nextInt((x - 50), (x + 50)), y, player.id);
+        item.options.add(new Item.ItemOption(73, 0));
         Service.getInstance().dropItemMap(player.zone, item);
-
     }
-//    public static boolean IsItemKhongChoGiaoDich(int id) {
-//        return (id >= 663 && id <= 667);
-//    }
 
+    /**
+     * Kiểm tra xem vật phẩm có thuộc loại không cho phép giao dịch hay không.
+     * @param id ID của vật phẩm
+     * @return True nếu vật phẩm không cho phép giao dịch, false nếu ngược lại
+     */
+    public static boolean IsItemKhongChoGiaoDich(int id) {
+        return (id >= 663 && id <= 667);
+    }
+
+    /**
+     * Tạo ngẫu nhiên vật phẩm đồ hủy diệt kích hoạt với các thuộc tính phù hợp.
+     * @param itemId ID của vật phẩm
+     * @param gender Giới tính của người chơi (0: Trái Đất, 1: Namec, 2: Xayda)
+     * @return Vật phẩm đồ hủy diệt đã được tạo
+     */
     public static Item randomCS_DHD(int itemId, int gender) {
         Item it = ItemService.gI().createItemSetKichHoat(itemId, 1);
         List<Integer> ao = Arrays.asList(650, 652, 654);
@@ -64,9 +93,7 @@ public class ArrietyDrop {
         List<Integer> gang = Arrays.asList(657, 659, 661);
         List<Integer> giay = Arrays.asList(658, 660, 662);
         int nhd = 656;
-        
-        int idOptionTD[] = new int[] {};
-        
+
         if (ao.contains(itemId)) {
             it.itemOptions.add(new Item.ItemOption(47, Util.highlightsItem(gender == 2, new Random().nextInt(1001) + 1800))); // áo từ 1800-2800 giáp
         }
@@ -80,11 +107,10 @@ public class ArrietyDrop {
             it.itemOptions.add(new Item.ItemOption(23, Util.highlightsItem(gender == 1, new Random().nextInt(11) + 80))); // ki 80-90k
         }
         if (nhd == itemId) {
-            it.itemOptions.add(new Item.ItemOption(14, new Random().nextInt(3) + 17)); //chí mạng 17-19%
+            it.itemOptions.add(new Item.ItemOption(14, new Random().nextInt(3) + 17)); // chí mạng 17-19%
         }
-        it.itemOptions.add(new Item.ItemOption(21, 80));// yêu cầu sm 80 tỉ
-        it.itemOptions.add(new Item.ItemOption(30, 1));// ko the gd
+        it.itemOptions.add(new Item.ItemOption(21, 80)); // yêu cầu sm 80 tỉ
+        it.itemOptions.add(new Item.ItemOption(30, 1)); // ko the gd
         return it;
     }
-
 }
