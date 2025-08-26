8aa4f6ac59715105dc3b4cbc9cf8a526cb80d3e4
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-26 13:22:10 +0700

    Update PonPut.java

--- Kaizv1.2/src/com/girlkun/models/boss/dhvt/PonPut.java
+++ Kaizv1.2/src/com/girlkun/models/boss/dhvt/PonPut.java
@@ -6,12 +6,15 @@ import com.girlkun.models.boss.BossesData;
 import com.girlkun.models.player.Player;
 
 /**
- * @author BTH sieu cap vippr0 
+ * Lớp đại diện cho boss PonPut trong sự kiện Đại Hội Võ Thuật (DHVT)
+ * @author Lucifer
  */
 public class PonPut extends BossDHVT {
 
+    /** Constructor khởi tạo boss PonPut với ID và dữ liệu từ BossesData */
     public PonPut(Player player) throws Exception {
         super(BossID.PON_PUT, BossesData.PON_PUT);
+        /** Gán người chơi mục tiêu cho boss */
         this.playerAtt = player;
     }
 }
