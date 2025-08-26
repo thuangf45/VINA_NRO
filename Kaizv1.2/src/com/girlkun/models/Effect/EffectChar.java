9065675f861d6bf9dff16f38972796d6f9a5d86b
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-26 09:10:19 +0700

    Update EffectChar

--- Kaizv1.2/src/com/girlkun/models/Effect/EffectChar.java
+++ Kaizv1.2/src/com/girlkun/models/Effect/EffectChar.java
@@ -1,23 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.girlkun.models.Effect;
 
 /**
- *
- * @author Administrator
+ * Lớp đại diện cho hiệu ứng của nhân vật trong game.
+ * @author Lucifer
  */
-public class EffectChar 
-{
+public class EffectChar {
+    
     public int id;
+    
     public int layer;
+    
     public int loop;
+    
     public int loopCount;
+    
     public int isStand;
-    public EffectChar(int id , int layer , int loop , int loopCount,int isStand)
-    {
+
+    /**
+     * Khởi tạo một hiệu ứng nhân vật với các thuộc tính được chỉ định.
+     * @param id ID của hiệu ứng
+     * @param layer Lớp hiển thị của hiệu ứng (độ ưu tiên hiển thị)
+     * @param loop Số lần lặp lại của hiệu ứng
+     * @param loopCount Số lần đã lặp lại của hiệu ứng
+     * @param isStand Trạng thái đứng yên (0: không đứng yên, 1: đứng yên)
+     */
+    public EffectChar(int id, int layer, int loop, int loopCount, int isStand) {
         this.id = id;
         this.layer = layer;
         this.loop = loop;
