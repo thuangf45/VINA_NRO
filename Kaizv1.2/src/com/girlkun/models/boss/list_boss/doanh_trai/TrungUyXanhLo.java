88386431b0c4c437c6253584e831a1720270ad6a
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-26 16:21:46 +0700

    Update TrungUyXanhLo

--- Kaizv1.2/src/com/girlkun/models/boss/list_boss/doanh_trai/TrungUyXanhLo.java
+++ Kaizv1.2/src/com/girlkun/models/boss/list_boss/doanh_trai/TrungUyXanhLo.java
@@ -1,4 +1,5 @@
 package com.girlkun.models.boss.list_boss.doanh_trai;
+
 import com.girlkun.consts.ConstPlayer;
 import com.girlkun.models.boss.*;
 import com.girlkun.models.map.ItemMap;
@@ -10,49 +11,78 @@ import com.girlkun.services.Service;
 import com.girlkun.utils.Util;
 
 /**
- * @author trùm
+ * Trung Uý Xanh Lơ - Boss trong Doanh Trại
+ * @author Lucifer
  */
 public class TrungUyXanhLo extends Boss {
 
     protected Player playerAtt;
+    private long st;
 
-    public TrungUyXanhLo(Zone zone, int dame, int hp  ) throws Exception {
+    /**
+     * Khởi tạo Trung Uý Xanh Lơ
+     * @param zone khu vực xuất hiện
+     * @param dame sát thương
+     * @param hp máu
+     * @throws Exception lỗi khởi tạo
+     */
+    public TrungUyXanhLo(Zone zone, int dame, int hp ) throws Exception {
         super(BossID.TRUNG_UY_XANH_LO, new BossData(
                 "Trung Uý Xanh Lơ", //name
                 ConstPlayer.TRAI_DAT, //gender
                 new short[]{135, 136, 137, -1, -1, -1}, //outfit {head, body, leg, bag, aura, eff}
-                ((hp/8 )), //dame
-                new int[]{((dame * 250 ))}, //hp
+                ((hp/8)), //dame
+                new int[]{((dame * 250))}, //hp
                 new int[]{62}, //map join
                 new int[][]{
-                {Skill.DEMON, 3, 1}, {Skill.DEMON, 6, 2}, {Skill.DRAGON, 7, 3}, {Skill.DRAGON, 1, 4}, {Skill.GALICK, 5, 5},
-                {Skill.KAMEJOKO, 7, 6}, {Skill.KAMEJOKO, 6, 7}, {Skill.KAMEJOKO, 5, 8}, {Skill.KAMEJOKO, 4, 9}, {Skill.KAMEJOKO, 3, 10}, {Skill.KAMEJOKO, 2, 11},{Skill.KAMEJOKO, 1, 12},
-              {Skill.ANTOMIC, 1, 13},  {Skill.ANTOMIC, 2, 14},  {Skill.ANTOMIC, 3, 15},{Skill.ANTOMIC, 4, 16},  {Skill.ANTOMIC, 5, 17},{Skill.ANTOMIC, 6, 19},  {Skill.ANTOMIC, 7, 20},
-                {Skill.MASENKO, 1, 21}, {Skill.MASENKO, 5, 22}, {Skill.MASENKO, 6, 23},
-                    {Skill.KAMEJOKO, 7, 1000},},
+                    {Skill.DEMON, 3, 1}, {Skill.DEMON, 6, 2}, {Skill.DRAGON, 7, 3}, 
+                    {Skill.DRAGON, 1, 4}, {Skill.GALICK, 5, 5},
+                    {Skill.KAMEJOKO, 7, 6}, {Skill.KAMEJOKO, 6, 7}, 
+                    {Skill.KAMEJOKO, 5, 8}, {Skill.KAMEJOKO, 4, 9}, 
+                    {Skill.KAMEJOKO, 3, 10}, {Skill.KAMEJOKO, 2, 11},
+                    {Skill.KAMEJOKO, 1, 12}, {Skill.ANTOMIC, 1, 13},  
+                    {Skill.ANTOMIC, 2, 14},  {Skill.ANTOMIC, 3, 15},
+                    {Skill.ANTOMIC, 4, 16},  {Skill.ANTOMIC, 5, 17},
+                    {Skill.ANTOMIC, 6, 19},  {Skill.ANTOMIC, 7, 20},
+                    {Skill.MASENKO, 1, 21}, {Skill.MASENKO, 5, 22}, 
+                    {Skill.MASENKO, 6, 23}, {Skill.KAMEJOKO, 7, 1000},
+                },
                 new String[]{}, //text chat 1
                 new String[]{"|-1|Nhóc con"}, //text chat 2
                 new String[]{}, //text chat 3
-                1
+                1 //respawn
         ));
         
         this.zone = zone;
     }
+
+    /**
+     * Rơi vật phẩm khi boss chết
+     * @param plKill người hạ gục
+     */
     @Override
     public void reward(Player plKill) {
         if (Util.isTrue(100, 100)) {
-            ItemMap it = new ItemMap(this.zone, 611, 1, this.location.x, this.zone.map.yPhysicInTop(this.location.x,
-                    this.location.y - 24), plKill.id);
+            ItemMap it = new ItemMap(this.zone, 611, 1, this.location.x, 
+                this.zone.map.yPhysicInTop(this.location.x, this.location.y - 24), plKill.id);
             Service.getInstance().dropItemMap(this.zone, it);
         }
     }
 
+    /**
+     * Hành động của boss trong vòng đời
+     */
+    @Override
     public void active() {
         super.active(); //To change body of generated methods, choose Tools | Templates.
         if (Util.canDoWithTime(st, 1800000)) {
             this.changeStatus(BossStatus.LEAVE_MAP);
         }
     }
+
+    /**
+     * Khi boss rời bản đồ
+     */
     @Override
     public void leaveMap() {
         super.leaveMap();
@@ -60,14 +90,25 @@ public class TrungUyXanhLo extends Boss {
             BossManager.gI().removeBoss(this);
         }
     }
+
+    /**
+     * Khi boss tham gia bản đồ
+     */
     @Override
     public void joinMap() {
         super.joinMap(); //To change body of generated methods, choose Tools | Templates.
         st = System.currentTimeMillis();
     }
-    private long st;
     
- //trùm
+    /**
+     * Boss chịu sát thương
+     * @param plAtt người tấn công
+     * @param damage sát thương
+     * @param piercing xuyên giáp
+     * @param isMobAttack có phải mob tấn công không
+     * @return lượng sát thương thực tế
+     */
+    // trùm
     public int injured(Player plAtt, int damage, boolean piercing, boolean isMobAttack) {
         if (!this.isDie()) {
             if (!piercing && Util.isTrue(this.nPoint.tlNeDon, 1000)) {
