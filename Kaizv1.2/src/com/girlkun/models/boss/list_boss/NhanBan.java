5a6475a132922a5db7b1cad67aba29ee8ee8038e
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-26 13:37:44 +0700

    Update NhanBan.java

--- Kaizv1.2/src/com/girlkun/models/boss/list_boss/NhanBan.java
+++ Kaizv1.2/src/com/girlkun/models/boss/list_boss/NhanBan.java
@@ -8,38 +8,60 @@ import com.girlkun.services.Service;
 import com.girlkun.utils.Util;
 
 /**
- * @Stole By Arriety
+ * Lớp đại diện cho boss Nhân Bản trong game.
+ * @author Lucifer
  */
 public class NhanBan extends Boss {
 
+    /**
+     * Constructor khởi tạo boss Nhân Bản với ID, dữ liệu boss và khu vực.
+     * @param bossID ID của boss
+     * @param bossData Dữ liệu cấu hình của boss
+     * @param zone Khu vực mà boss xuất hiện
+     * @throws Exception Nếu có lỗi trong quá trình khởi tạo
+     */
     public NhanBan(int bossID, BossData bossData, Zone zone) throws Exception {
         super(bossID, bossData);
         this.zone = zone;
     }
 
+    /**
+     * Xử lý phần thưởng khi người chơi tiêu diệt boss Nhân Bản.
+     * @param plKill Người chơi đã tiêu diệt boss
+     */
     @Override
-public void reward(Player plKill) {
-    if (!plKill.getSession().actived) {
-      //  Service.gI().sendThongBao(plKill, "Vui lòng kích hoạt tài khoản để sử dụng chức năng này");
-    } else {
-        int[] itemIds = {1099, 1100, 1101, 1102, 1103, 1978, 1235, 1234, 1233}; // Các ID của vật phẩm cần rơi
-        for (int itemId : itemIds) {
-            ItemMap it = new ItemMap(this.zone, itemId, Util.nextInt(1, 2), this.location.x, this.zone.map.yPhysicInTop(this.location.x,
-                    this.location.y - 24), plKill.id);
-            Service.gI().dropItemMap(this.zone, it);
+    public void reward(Player plKill) {
+        if (!plKill.getSession().actived) {
+            Service.gI().sendThongBao(plKill, "Vui lòng kích hoạt tài khoản để sử dụng chức năng này");
+        } else {
+            int[] itemIds = {1099, 1100, 1101, 1102, 1103, 1978, 1235, 1234, 1233}; // Các ID của vật phẩm cần rơi
+            for (int itemId : itemIds) {
+                ItemMap it = new ItemMap(this.zone, itemId, Util.nextInt(1, 2), this.location.x, this.zone.map.yPhysicInTop(this.location.x,
+                        this.location.y - 24), plKill.id);
+                Service.gI().dropItemMap(this.zone, it);
+            }
         }
     }
-}
+
+    /**
+     * Kích hoạt hành vi của boss Nhân Bản.
+     */
     @Override
     public void active() {
         super.active();
     }
 
+    /**
+     * Thêm boss Nhân Bản vào bản đồ.
+     */
     @Override
     public void joinMap() {
         super.joinMap();
     }
 
+    /**
+     * Xóa boss Nhân Bản khỏi bản đồ và thực hiện dọn dẹp tài nguyên.
+     */
     @Override
     public void leaveMap() {
         super.leaveMap();
@@ -47,3 +69,4 @@ public void reward(Player plKill) {
         this.dispose();
     }
 }
+
