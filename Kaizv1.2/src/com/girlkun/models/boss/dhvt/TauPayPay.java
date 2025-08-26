7e81581f19ad7d3233b2280e5c26cb24ee9177e7
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-26 13:22:15 +0700

    Update TauPayPay.java

--- Kaizv1.2/src/com/girlkun/models/boss/dhvt/TauPayPay.java
+++ Kaizv1.2/src/com/girlkun/models/boss/dhvt/TauPayPay.java
@@ -6,12 +6,15 @@ import com.girlkun.models.boss.BossesData;
 import com.girlkun.models.player.Player;
 
 /**
- * @author BTH sieu cap vippr0 
+ * Lớp đại diện cho boss TauPayPay trong sự kiện Đại Hội Võ Thuật (DHVT)
+ * @author Lucifer
  */
 public class TauPayPay extends BossDHVT {
 
+    /** Constructor khởi tạo boss TauPayPay với ID và dữ liệu từ BossesData */
     public TauPayPay(Player player) throws Exception {
         super(BossID.TAU_PAY_PAY, BossesData.TAU_PAY_PAY);
+        /** Gán người chơi mục tiêu cho boss */
         this.playerAtt = player;
     }
 }
