39711e238d481f18ce99f37902211e87a6d12ae3
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-26 13:22:17 +0700

    Update ThienXinHang.java

--- Kaizv1.2/src/com/girlkun/models/boss/dhvt/ThienXinHang.java
+++ Kaizv1.2/src/com/girlkun/models/boss/dhvt/ThienXinHang.java
@@ -8,40 +8,49 @@ import com.girlkun.services.EffectSkillService;
 import com.girlkun.utils.Util;
 
 /**
- * @author BTH sieu cap vippr0 
+ * Lớp đại diện cho boss ThienXinHang trong sự kiện Đại Hội Võ Thuật (DHVT)
+ * @author Lucifer
  */
 public class ThienXinHang extends BossDHVT {
 
+    /** Thời gian lần cuối sử dụng kỹ năng phân thân */
     private long lastTimePhanThan = System.currentTimeMillis();
 
+    /** Constructor khởi tạo boss ThienXinHang với ID và dữ liệu từ BossesData */
     public ThienXinHang(Player player) throws Exception {
         super(BossID.THIEN_XIN_HANG, BossesData.THIEN_XIN_HANG);
+        /** Gán người chơi mục tiêu cho boss */
         this.playerAtt = player;
     }
 
-
+    /** Tấn công người chơi, bao gồm xóa hiệu ứng choáng và sử dụng kỹ năng phân thân */
     @Override
     public void attack() {
         super.attack();
         try {
+            /** Xóa hiệu ứng choáng (stun) khỏi boss */
             EffectSkillService.gI().removeStun(this);
+            /** Kiểm tra xem đã đủ 30 giây kể từ lần cuối sử dụng phân thân chưa */
             if (Util.canDoWithTime(lastTimePhanThan, 30000)) {
                 lastTimePhanThan = System.currentTimeMillis();
+                /** Gọi phương thức phân thân để tạo các bản sao */
                 phanThan();
             }
         } catch (Exception ex) {
-            
+            /** Xử lý ngoại lệ (để trống trong mã gốc) */
         }
     }
 
+    /** Tạo các bản sao của boss ThienXinHang */
     private void phanThan() {
         try {
+            /** Tạo bốn bản sao của boss với các ID khác nhau */
             new ThienXinHangClone(BossID.THIEN_XIN_HANG_CLONE, playerAtt);
             new ThienXinHangClone(BossID.THIEN_XIN_HANG_CLONE1, playerAtt);
             new ThienXinHangClone(BossID.THIEN_XIN_HANG_CLONE2, playerAtt);
             new ThienXinHangClone(BossID.THIEN_XIN_HANG_CLONE3, playerAtt);
         } catch (Exception e) {
-            
+            /** Xử lý ngoại lệ (để trống trong mã gốc) */
         }
     }
 }
