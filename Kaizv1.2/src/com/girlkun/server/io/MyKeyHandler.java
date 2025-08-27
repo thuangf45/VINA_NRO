2ebd6bc24a021494b59622b33d6186b576d260ba
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-28 00:18:37 +0700

    Update MyKeyHandler

--- Kaizv1.2/src/com/girlkun/server/io/MyKeyHandler.java
+++ Kaizv1.2/src/com/girlkun/server/io/MyKeyHandler.java
@@ -4,35 +4,22 @@ import com.girlkun.data.DataGame;
 import com.girlkun.network.session.ISession;
 import com.girlkun.network.example.KeyHandler;
 
-
+/**
+ * Lớp MyKeyHandler xử lý việc gửi khóa (key) cho phiên kết nối của client.
+ * Kế thừa từ lớp KeyHandler và mở rộng chức năng để gửi thông tin phiên bản tài nguyên.
+ * 
+ * @author Lucifer
+ */
 public class MyKeyHandler extends KeyHandler {
 
+    /**
+     * Gửi khóa cho phiên kết nối và gửi thông tin phiên bản tài nguyên tới client.
+     * 
+     * @param session Phiên kết nối của client.
+     */
     @Override
     public void sendKey(ISession session) {
         super.sendKey(session);
         DataGame.sendVersionRes((MySession) session);
     }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
