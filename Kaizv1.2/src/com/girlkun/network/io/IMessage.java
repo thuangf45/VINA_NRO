e2f13d79d35b14c6f261969c14019ca60661106b
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-27 23:02:42 +0700

    Update IMessage

--- Kaizv1.2/src/com/girlkun/network/io/IMessage.java
+++ Kaizv1.2/src/com/girlkun/network/io/IMessage.java
@@ -3,20 +3,42 @@ package com.girlkun.network.io;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 
-public interface IMessage extends IIOMessage {
-  DataOutputStream writer();
-  
-  DataInputStream reader();
-  
-  byte[] getData();
-  
-  void cleanup();
-  
-  void dispose();
-}
-
-
-/* Location:              C:\Users\VoHoangKiet\Downloads\TEA_V5\lib\GirlkunNetwork.jar!\com\girlkun\network\io\IMessage.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
+/**
+ * Giao diện mở rộng từ IIOMessage, cung cấp các phương thức để quản lý luồng dữ liệu đầu vào và đầu ra,
+ * lấy dữ liệu dạng mảng byte, và dọn dẹp tài nguyên sau khi sử dụng.
+ *
+ * @author Lucifer
  */
+public interface IMessage extends IIOMessage {
+
+    /**
+     * Trả về đối tượng DataOutputStream để ghi dữ liệu vào luồng đầu ra.
+     *
+     * @return đối tượng DataOutputStream
+     */
+    DataOutputStream writer();
+
+    /**
+     * Trả về đối tượng DataInputStream để đọc dữ liệu từ luồng đầu vào.
+     *
+     * @return đối tượng DataInputStream
+     */
+    DataInputStream reader();
+
+    /**
+     * Lấy dữ liệu dưới dạng mảng byte từ thông điệp.
+     *
+     * @return mảng byte chứa dữ liệu của thông điệp
+     */
+    byte[] getData();
+
+    /**
+     * Dọn dẹp các tài nguyên liên quan đến thông điệp, nhưng không giải phóng hoàn toàn.
+     */
+    void cleanup();
+
+    /**
+     * Giải phóng hoàn toàn các tài nguyên liên quan đến thông điệp.
+     */
+    void dispose();
+}
