95e76e15daba0333f3d3e768d61a5b6ba9e6a214
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-27 22:27:28 +0700

    Update TaskPlayer

--- Kaizv1.2/src/com/girlkun/models/task/TaskPlayer.java
+++ Kaizv1.2/src/com/girlkun/models/task/TaskPlayer.java
@@ -1,20 +1,29 @@
 package com.girlkun.models.task;
 
-
-
+/**
+ * Lớp quản lý nhiệm vụ của người chơi, bao gồm nhiệm vụ chính và nhiệm vụ phụ.
+ * @author Lucifer
+ */
 public class TaskPlayer {
 
+    /** Nhiệm vụ chính của người chơi. */
     public TaskMain taskMain;
-    
+
+    /** Nhiệm vụ phụ của người chơi. */
     public SideTask sideTask;
 
+    /**
+     * Khởi tạo đối tượng TaskPlayer với nhiệm vụ phụ mặc định.
+     */
     public TaskPlayer() {
         this.sideTask = new SideTask();
     }
-    
-    public void dispose(){
+
+    /**
+     * Giải phóng tài nguyên của đối tượng TaskPlayer.
+     */
+    public void dispose() {
         this.taskMain = null;
         this.sideTask = null;
     }
-
 }
