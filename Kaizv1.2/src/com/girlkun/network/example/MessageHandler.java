cdeaca9c8992830820503f0b0b0f4d67ad343e83
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-27 22:39:03 +0700

    Update MessageHandler

--- Kaizv1.2/src/com/girlkun/network/example/MessageHandler.java
+++ Kaizv1.2/src/com/girlkun/network/example/MessageHandler.java
@@ -1,25 +1,30 @@
-/*    */ package com.girlkun.network.example;
-/*    */ 
-/*    */ import com.girlkun.network.handler.IMessageHandler;
-/*    */ import com.girlkun.network.io.Message;
-/*    */ import com.girlkun.network.session.ISession;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public class MessageHandler
-/*    */   implements IMessageHandler
-/*    */ {
-/*    */   public void onMessage(ISession session, Message msg) throws Exception {
-/* 16 */     System.out.println(msg.reader().readUTF());
-/* 17 */     msg.cleanup();
-/*    */   }
-/*    */ }
+package com.girlkun.network.example;
 
+import com.girlkun.network.handler.IMessageHandler;
+import com.girlkun.network.io.Message;
+import com.girlkun.network.session.ISession;
 
-/* Location:              C:\Users\VoHoangKiet\Downloads\TEA_V5\lib\GirlkunNetwork.jar!\com\girlkun\network\example\MessageHandler.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
+/**
+ * Lớp xử lý các tin nhắn mạng nhận được trong game.
+ * @author Lucifer
  */
+public class MessageHandler implements IMessageHandler {
+
+    /**
+     * Xử lý tin nhắn nhận được từ phiên kết nối mạng.
+     *
+     * @param session Phiên kết nối mạng.
+     * @param msg Tin nhắn nhận được.
+     * @throws Exception Nếu xảy ra lỗi trong quá trình xử lý tin nhắn.
+     */
+    @Override
+    public void onMessage(ISession session, Message msg) throws Exception {
+        try {
+            System.out.println(msg.reader().readUTF());
+            msg.cleanup();
+        } catch (Exception e) {
+
+            throw e;
+        }
+    }
+}
