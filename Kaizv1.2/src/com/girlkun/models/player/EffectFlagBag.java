2ebf235607e427b57e8adba9befd709d1b6dab4f
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-27 11:38:42 +0700

    Update EffectFlagBag

--- Kaizv1.2/src/com/girlkun/models/player/EffectFlagBag.java
+++ Kaizv1.2/src/com/girlkun/models/player/EffectFlagBag.java
@@ -1,19 +1,43 @@
 package com.girlkun.models.player;
 
-
+/**
+ * Lớp đại diện cho các cờ hiệu ứng (flag effects) của túi đồ người chơi, 
+ * quản lý trạng thái sử dụng các vật phẩm hiệu ứng đặc biệt.
+ * @author Lucifer
+ */
 public class EffectFlagBag {
+    
+    /** Trạng thái sử dụng vật phẩm Võ Ốc. */
     public boolean useVoOc;
+
+    /** Trạng thái sử dụng vật phẩm Cây Kem. */
     public boolean useCayKem;
+
+    /** Trạng thái sử dụng vật phẩm Cá Heo. */
     public boolean useCaHeo;
+
+    /** Trạng thái sử dụng vật phẩm Con Diều. */
     public boolean useConDieu;
+
+    /** Trạng thái sử dụng vật phẩm Diều Rồng. */
     public boolean useDieuRong;
+
+    /** Trạng thái sử dụng vật phẩm Mèo Mùn. */
     public boolean useMeoMun;
+
+    /** Trạng thái sử dụng vật phẩm Xiên Cá. */
     public boolean useXienCa;
+
+    /** Trạng thái sử dụng vật phẩm Phong Heo. */
     public boolean usePhongHeo;
+
+    /** Trạng thái sử dụng vật phẩm Hào Quang. */
     public boolean useHaoQuang;
 
+    /**
+     * Đặt lại tất cả trạng thái sử dụng vật phẩm hiệu ứng về false.
+     */
     public void reset() {
-        this.useVoOc = false;
         this.useVoOc = false;
         this.useCayKem = false;
         this.useCaHeo = false;
@@ -25,6 +49,9 @@ public class EffectFlagBag {
         this.useHaoQuang = false;
     }
     
-    public void dispose(){
+    /**
+     * Giải phóng tài nguyên hoặc thiết lập lại trạng thái của các cờ hiệu ứng.
+     */
+    public void dispose() {
     }
 }
