76cc55491ae36ee188694c76f9acc90f183beb32
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-27 22:36:36 +0700

    Update KeyHandler

--- Kaizv1.2/src/com/girlkun/network/example/KeyHandler.java
+++ Kaizv1.2/src/com/girlkun/network/example/KeyHandler.java
@@ -1,54 +1,62 @@
-/*    */ package com.girlkun.network.example;
-/*    */ 
+package com.girlkun.network.example;
 
-/*    */ import com.girlkun.network.handler.IKeySessionHandler;
-/*    */ import com.girlkun.network.io.Message;
-/*    */ import com.girlkun.network.session.ISession;
+import com.girlkun.network.handler.IKeySessionHandler;
+import com.girlkun.network.io.Message;
+import com.girlkun.network.session.ISession;
 import com.girlkun.network.CommandMessage;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public class KeyHandler
-/*    */   implements IKeySessionHandler
-/*    */ {
-/*    */   public void sendKey(ISession session) {
-/* 17 */     Message msg = new Message(CommandMessage.REQUEST_KEY);
-/*    */     try {
-/* 19 */       byte[] KEYS = session.getKey();
-/* 20 */       msg.writer().writeByte(KEYS.length);
-/* 21 */       msg.writer().writeByte(KEYS[0]);
-/* 22 */       for (int i = 1; i < KEYS.length; i++) {
-/* 23 */         msg.writer().writeByte(KEYS[i] ^ KEYS[i - 1]);
-/*    */       }
-/* 25 */       session.doSendMessage(msg);
-/* 26 */       msg.cleanup();
-/* 27 */       session.setSentKey(true);
-/* 28 */     } catch (Exception exception) {}
-/*    */   }
-/*    */ 
-/*    */ 
-/*    */   
-/*    */   public void setKey(ISession session, Message message) throws Exception {
-/*    */     try {
-/* 35 */       byte b = message.reader().readByte();
-/* 36 */       byte[] KEYS = new byte[b];
-/* 37 */       for (int i = 0; i < b; i++) {
-/* 38 */         KEYS[i] = message.reader().readByte();
-/*    */       }
-/* 40 */       for (int j = 0; j < KEYS.length - 1; j++) {
-/* 41 */         KEYS[j + 1] = (byte)(KEYS[j + 1] ^ KEYS[j]);
-/*    */       }
-/* 43 */       session.setKey(KEYS);
-/* 44 */       session.setSentKey(true);
-/* 45 */     } catch (Exception exception) {}
-/*    */   }
-/*    */ }
 
-
-/* Location:              C:\Users\VoHoangKiet\Downloads\TEA_V5\lib\GirlkunNetwork.jar!\com\girlkun\network\example\KeyHandler.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
+/**
+ * Lớp xử lý các thao tác liên quan đến khóa mã hóa trong giao tiếp mạng của game.
+ * @author Lucifer
  */
+public class KeyHandler implements IKeySessionHandler {
+
+    /**
+     * Gửi khóa mã hóa đến phiên kết nối.
+     *
+     * @param session Phiên kết nối mạng.
+     */
+    @Override
+    public void sendKey(ISession session) {
+        Message msg = new Message(CommandMessage.REQUEST_KEY);
+        try {
+            byte[] KEYS = session.getKey();
+            msg.writer().writeByte(KEYS.length);
+            msg.writer().writeByte(KEYS[0]);
+            for (int i = 1; i < KEYS.length; i++) {
+                msg.writer().writeByte(KEYS[i] ^ KEYS[i - 1]);
+            }
+            session.doSendMessage(msg);
+            msg.cleanup();
+            session.setSentKey(true);
+        } catch (Exception exception) {
+
+        }
+    }
+
+    /**
+     * Thiết lập khóa mã hóa cho phiên kết nối từ tin nhắn nhận được.
+     *
+     * @param session Phiên kết nối mạng.
+     * @param message Tin nhắn chứa dữ liệu khóa.
+     * @throws Exception Nếu xảy ra lỗi trong quá trình đọc dữ liệu.
+     */
+    @Override
+    public void setKey(ISession session, Message message) throws Exception {
+        try {
+            byte b = message.reader().readByte();
+            byte[] KEYS = new byte[b];
+            for (int i = 0; i < b; i++) {
+                KEYS[i] = message.reader().readByte();
+            }
+            for (int j = 0; j < KEYS.length - 1; j++) {
+                KEYS[j + 1] = (byte)(KEYS[j + 1] ^ KEYS[j]);
+            }
+            session.setKey(KEYS);
+            session.setSentKey(true);
+        } catch (Exception exception) {
+
+            throw exception;
+        }
+    }
+}
