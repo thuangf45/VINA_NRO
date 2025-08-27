67f56390ed4a4568db4abfb43937baa15c133e13
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-27 23:16:23 +0700

    Update Ikey

--- Kaizv1.2/src/com/girlkun/network/session/IKey.java
+++ Kaizv1.2/src/com/girlkun/network/session/IKey.java
@@ -2,22 +2,54 @@ package com.girlkun.network.session;
 
 import com.girlkun.network.io.Message;
 
-public interface IKey {
-  void sendKey() throws Exception;
-  
-  void setKey(Message paramMessage) throws Exception;
-  
-  void setKey(byte[] paramArrayOfbyte);
-  
-  byte[] getKey();
-  
-  boolean sentKey();
-  
-  void setSentKey(boolean paramBoolean);
-}
-
-
-/* Location:              C:\Users\VoHoangKiet\Downloads\TEA_V5\lib\GirlkunNetwork.jar!\com\girlkun\network\session\IKey.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
+/**
+ * Giao diện định nghĩa các phương thức để quản lý khóa mã hóa trong phiên làm việc mạng.
+ * Hỗ trợ gửi, thiết lập, lấy khóa và kiểm tra trạng thái gửi khóa.
+ *
+ * @author Lucifer
  */
+public interface IKey {
+
+    /**
+     * Gửi khóa mã hóa đến phiên làm việc.
+     *
+     * @throws Exception nếu xảy ra lỗi trong quá trình gửi
+     */
+    void sendKey() throws Exception;
+
+    /**
+     * Thiết lập khóa mã hóa từ một thông điệp.
+     *
+     * @param paramMessage thông điệp chứa dữ liệu khóa
+     * @throws Exception nếu xảy ra lỗi trong quá trình thiết lập
+     */
+    void setKey(Message paramMessage) throws Exception;
+
+    /**
+     * Thiết lập khóa mã hóa từ một mảng byte.
+     *
+     * @param paramArrayOfbyte mảng byte chứa dữ liệu khóa
+     */
+    void setKey(byte[] paramArrayOfbyte);
+
+    /**
+     * Lấy khóa mã hóa hiện tại.
+     *
+     * @return mảng byte chứa dữ liệu khóa
+     */
+    byte[] getKey();
+
+    /**
+     * Kiểm tra xem khóa đã được gửi hay chưa.
+     *
+     * @return true nếu khóa đã được gửi, false nếu chưa
+     */
+    boolean sentKey();
+
+    /**
+     * Thiết lập trạng thái gửi khóa.
+     *
+     * @param paramBoolean true nếu khóa đã được gửi, false nếu chưa
+     */
+    void setSentKey(boolean paramBoolean);
+}
