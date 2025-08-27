61a978402ea7d2893d5c30465a8a82b51944aba1
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-27 22:34:59 +0700

    Update DFSendCollect

--- Kaizv1.2/src/com/girlkun/network/example/DFSendCollect.java
+++ Kaizv1.2/src/com/girlkun/network/example/DFSendCollect.java
@@ -1,63 +1,87 @@
-/*    */ package com.girlkun.network.example;
-/*    */ 
-/*    */ import com.girlkun.network.handler.IMessageSendCollect;
-/*    */ import com.girlkun.network.io.Message;
-/*    */ import com.girlkun.network.session.ISession;
-/*    */ import java.io.DataInputStream;
-/*    */ import java.io.DataOutputStream;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public class DFSendCollect
-/*    */   implements IMessageSendCollect
-/*    */ {
-/*    */   public Message readMessage(ISession session, DataInputStream dis) throws Exception {
-/* 18 */     byte cmd = dis.readByte();
-/* 19 */     int size = dis.readInt();
-/* 20 */     byte[] data = new byte[size];
-/* 21 */     int len = 0;
-/* 22 */     int byteRead = 0;
-/* 23 */     while (len != -1 && byteRead < size) {
-/* 24 */       len = dis.read(data, byteRead, size - byteRead);
-/* 25 */       if (len > 0) {
-/* 26 */         byteRead += len;
-/*    */       }
-/*    */     } 
-/* 29 */     return new Message(cmd, data);
-/*    */   }
-/*    */ 
-/*    */   
-/*    */   public byte readKey(ISession session, byte b) {
-/* 34 */     return -1;
-/*    */   }
-/*    */ 
-/*    */   
-/*    */   public void doSendMessage(ISession session, DataOutputStream dos, Message msg) throws Exception {
-/*    */     try {
-/* 40 */       byte[] data = msg.getData();
-/* 41 */       dos.writeByte(msg.command);
-/* 42 */       if (data != null) {
-/* 43 */         dos.writeInt(data.length);
-/* 44 */         dos.write(data);
-/*    */       } else {
-/* 46 */         dos.writeInt(0);
-/*    */       } 
-/* 48 */       dos.flush();
-/* 49 */     } catch (Exception exception) {}
-/*    */   }
-/*    */ 
-/*    */ 
-/*    */   
-/*    */   public byte writeKey(ISession session, byte b) {
-/* 55 */     return -1;
-/*    */   }
-/*    */ }
+package com.girlkun.network.example;
 
+import com.girlkun.network.handler.IMessageSendCollect;
+import com.girlkun.network.io.Message;
+import com.girlkun.network.session.ISession;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 
-/* Location:              C:\Users\VoHoangKiet\Downloads\TEA_V5\lib\GirlkunNetwork.jar!\com\girlkun\network\example\DFSendCollect.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
+/**
+ * Lớp thực hiện việc thu thập và gửi tin nhắn qua mạng trong game.
+ * @author Lucifer
  */
+public class DFSendCollect implements IMessageSendCollect {
+
+    /**
+     * Đọc dữ liệu tin nhắn từ luồng đầu vào và tạo đối tượng Message.
+     *
+     * @param session Phiên kết nối mạng.
+     * @param dis Luồng dữ liệu đầu vào.
+     * @return Đối tượng Message chứa dữ liệu đã đọc.
+     * @throws Exception Nếu xảy ra lỗi trong quá trình đọc dữ liệu.
+     */
+    @Override
+    public Message readMessage(ISession session, DataInputStream dis) throws Exception {
+        byte cmd = dis.readByte();
+        int size = dis.readInt();
+        byte[] data = new byte[size];
+        int len = 0;
+        int byteRead = 0;
+        while (len != -1 && byteRead < size) {
+            len = dis.read(data, byteRead, size - byteRead);
+            if (len > 0) {
+                byteRead += len;
+            }
+        }
+        return new Message(cmd, data);
+    }
+
+    /**
+     * Đọc khóa mã hóa từ dữ liệu đầu vào.
+     *
+     * @param session Phiên kết nối mạng.
+     * @param b Byte cần đọc khóa.
+     * @return Giá trị khóa, mặc định trả về -1.
+     */
+    @Override
+    public byte readKey(ISession session, byte b) {
+        return -1;
+    }
+
+    /**
+     * Gửi tin nhắn qua luồng đầu ra.
+     *
+     * @param session Phiên kết nối mạng.
+     * @param dos Luồng dữ liệu đầu ra.
+     * @param msg Tin nhắn cần gửi.
+     * @throws Exception Nếu xảy ra lỗi trong quá trình gửi dữ liệu.
+     */
+    @Override
+    public void doSendMessage(ISession session, DataOutputStream dos, Message msg) throws Exception {
+        try {
+            byte[] data = msg.getData();
+            dos.writeByte(msg.command);
+            if (data != null) {
+                dos.writeInt(data.length);
+                dos.write(data);
+            } else {
+                dos.writeInt(0);
+            }
+            dos.flush();
+        } catch (Exception exception) {
+
+        }
+    }
+
+    /**
+     * Ghi khóa mã hóa cho dữ liệu đầu ra.
+     *
+     * @param session Phiên kết nối mạng.
+     * @param b Byte cần ghi khóa.
+     * @return Giá trị khóa, mặc định trả về -1.
+     */
+    @Override
+    public byte writeKey(ISession session, byte b) {
+        return -1;
+    }
+}
