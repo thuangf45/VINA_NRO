52b827f590783e792a8f70c24b33aadf6b899394
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-27 23:23:50 +0700

    Update SessionFactory

--- Kaizv1.2/src/com/girlkun/network/session/SessionFactory.java
+++ Kaizv1.2/src/com/girlkun/network/session/SessionFactory.java
@@ -1,31 +1,41 @@
-/*    */ package com.girlkun.network.session;
-/*    */ 
-/*    */ import java.net.Socket;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public class SessionFactory
-/*    */ {
-/*    */   private static SessionFactory I;
-/*    */   
-/*    */   public static SessionFactory gI() {
-/* 15 */     if (I == null) {
-/* 16 */       I = new SessionFactory();
-/*    */     }
-/* 18 */     return I;
-/*    */   }
-/*    */ 
-/*    */   
-/*    */   public ISession cloneSession(Class<ISession> clazz, Socket socket) throws Exception {
-/* 23 */     return clazz.getConstructor(new Class[] { Socket.class }).newInstance(new Object[] { socket });
-/*    */   }
-/*    */ }
+package com.girlkun.network.session;
 
+import java.net.Socket;
 
-/* Location:              C:\Users\VoHoangKiet\Downloads\TEA_V5\lib\GirlkunNetwork.jar!\com\girlkun\network\session\SessionFactory.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
+/**
+ * Lớp thực hiện mẫu thiết kế Factory để tạo các phiên làm việc mạng.
+ * Hỗ trợ tạo bản sao của các phiên làm việc dựa trên lớp được chỉ định.
+ *
+ * @author Lucifer
  */
+public class SessionFactory {
+
+    /**
+     * Thể hiện duy nhất của lớp SessionFactory (singleton pattern).
+     */
+    private static SessionFactory I;
+
+    /**
+     * Lấy thể hiện duy nhất của SessionFactory (singleton pattern).
+     *
+     * @return thể hiện SessionFactory
+     */
+    public static SessionFactory gI() {
+        if (I == null) {
+            I = new SessionFactory();
+        }
+        return I;
+    }
+
+    /**
+     * Tạo một bản sao phiên làm việc từ lớp được chỉ định và socket.
+     *
+     * @param clazz  lớp của phiên làm việc (phải triển khai ISession)
+     * @param socket socket kết nối với client
+     * @return phiên làm việc được tạo
+     * @throws Exception nếu lớp không hợp lệ hoặc không thể tạo instance
+     */
+    public ISession cloneSession(Class<ISession> clazz, Socket socket) throws Exception {
+        return clazz.getConstructor(new Class[] { Socket.class }).newInstance(new Object[] { socket });
+    }
+}
