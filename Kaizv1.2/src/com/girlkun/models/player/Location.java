7747025a841e694be808c1aeafffe3fd038696a7
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-27 19:19:58 +0700

    Update Location

--- Kaizv1.2/src/com/girlkun/models/player/Location.java
+++ Kaizv1.2/src/com/girlkun/models/player/Location.java
@@ -1,10 +1,23 @@
 package com.girlkun.models.player;
 
-
+/**
+ * Lớp Location lưu trữ thông tin vị trí và thời gian di chuyển của người chơi trong trò chơi.
+ * @author Lucifer
+ */
 public class Location {
 
+    /**
+     * Tọa độ x của người chơi trên bản đồ.
+     */
     public int x;
+
+    /**
+     * Tọa độ y của người chơi trên bản đồ.
+     */
     public int y;
 
+    /**
+     * Thời điểm lần cuối người chơi di chuyển (mili giây).
+     */
     public long lastTimeplayerMove;
 }
